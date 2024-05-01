2297f39137d8031557970d980154d9f746aa1581
Dushenko <Dushenko@broccolidev.ru>
2024-05-01 14:41:18 +0700

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests as req
+import json
 from config import ACCESSTOKEN, YANDEXTOKEN
 
 
@@ -8,6 +9,8 @@ class VK:
         self.token = token
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version, 'album_id': 'profile', 'extended': 1}
+        # Свойство для хранения счетчика лайков каждого элемента
+        self.last_likes_count = None
 
 
     def get_photos(self):
@@ -20,15 +23,33 @@ class VK:
    
     
     def save_photos(self, data):
+         saved_photos_info = []
          if 'response' in data:
             photos = data['response']['items']
             for photo in photos:
+                
                 likes = photo['likes']['count']
+
+                if self.last_likes_count is not None and likes == self.last_likes_count:
+                    likes = photo['likes']['count'] + photo['date']
+
                 sizes = photo['sizes']
                 photo_url = next((size['url'] for size in sizes if size['type'] in ['z']), None)
                 path = f'/Netology/{likes}'
                 yandex = YandexRepository(path)
                 yandex.yandex_save(photo_url, YANDEXTOKEN)                 
+                self.last_likes_count = likes
+
+                # Сохраняем информацию о файле
+                obj = {
+                    "file_name": f'{likes}.jpg',
+                    "size": 'z'
+                }
+                saved_photos_info.append(obj)
+
+         with open('json_data.json', 'w') as json_file:
+                json.dump(saved_photos_info, json_file, indent=4)
+                print(f"Файл записан: {json_file}.")
 
 
 class YandexRepository:
@@ -57,23 +78,18 @@ class YandexRepository:
             if response.status_code == 202:
                 upload_href = response_data['href']
                 if upload_href:
-                    print(f"File upload initiated. Upload link: {upload_href}")
+                    print(f"Файл загружен на диск: {upload_href}")
                 else:
-                    print("Upload link not found in response")
+                    print("Файл не найден")
             else:
-                print(f"Error message: {response_data.get('message')}")
+                print(f"Ошибка: {response_data.get('message')}")
         except req.exceptions.RequestException as e:
-            print(f"Error making request: {e}")
+            print(f"Ошибка: {e}")
             
-        
-
-        
 
 access_token = ACCESSTOKEN
 owner_id = 7770471
 vk = VK(access_token, owner_id)
-
 data = vk.get_photos()
-
 vk.save_photos(data)
 
