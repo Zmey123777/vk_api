7d4d57e52bc338ea88559105cb064b836719b00d
Dushenko <Dushenko@broccolidev.ru>
2024-05-01 15:43:53 +0700

    fixes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests as req
+from typing import Dict, Any
 import json
 import logging
 from config import VKACCESSTOKEN, YANDEXTOKEN
 
 
-# Класс для получения и записи фотографий профия Вконтакте
+# Класс для получения и записи фотографий профиля Вконтакте
 class VK:
     def __init__(self, token, owner_id, version='5.131') -> None:
         self.owner_id = owner_id
@@ -14,33 +15,30 @@ class VK:
         # Свойство для хранения счетчика лайков каждого элемента
         self.last_likes_count = None
 
-
-    def get_photos(self):
+    # Метод для получения фотографий из API VK
+    def get_photos(self) -> Dict[str, Any]:
         url = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self.owner_id}
-
         response = req.get(url, params={**self.params, **params})
 
         return response.json()
    
-    # Основной метод программы который записывает полученные фотографии
-    def save_photos(self, data) -> None:
+    # Основной метод программы который обрабатывает и записывает полученные фотографии
+    def handle_photos(self, data) -> None:
+         yandex = YandexRepository(YANDEXTOKEN)
          saved_photos_info = []
          if 'response' in data:
             photos = data['response']['items']
             for photo in photos:
-                
                 likes = photo['likes']['count']
-
                 if self.last_likes_count is not None and likes == self.last_likes_count:
                     likes = photo['likes']['count'] + photo['date']
 
                 sizes = photo['sizes']
                 photo_url = next((size['url'] for size in sizes if size['type'] in ['z']), None)
                 path = f'/Netology/{likes}'
-                yandex = YandexRepository(path)
                 logging.info('Отправка файла в Яндекс репозиторий')
-                yandex.yandex_save(photo_url, YANDEXTOKEN)                 
+                yandex.yandex_save(photo_url, path)               
                 self.last_likes_count = likes
 
                 # Сохраняем информацию о файле
@@ -55,23 +53,23 @@ class VK:
                 logging.debug(f" Отладочный файл записан: {json_file}.")
 
 
-# Класс для записи фотографий в Яндекс репозиторий
+# Класс для работы с API Yandex
 class YandexRepository:
-    def __init__(self, path) -> None:
-        self.path = path
+    def __init__(self,token) -> None:
+        self.token = token
     
     # Метод использующий Яндекс REST API для записи фотографий на диск Папка /Netology
-    def yandex_save(self, url, auth) -> None:
+    def yandex_save(self, url, path) -> None:
         api_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 
         headers = {
-            'Authorization': f'OAuth {auth}',
+            'Authorization': f'OAuth {self.token}',
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
 
         params = {
-        'path': self.path,
+        'path': path,
         'url': url
         }
 
@@ -94,8 +92,9 @@ class YandexRepository:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
+    # Тестовый вызов
     access_token = VKACCESSTOKEN
     owner_id = 7770471
     vk = VK(access_token, owner_id)
     data = vk.get_photos()
-    vk.save_photos(data)
+    vk.handle_photos(data)
